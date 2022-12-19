e1224a26ad24787a888416981f77cdffc952c101
ozmld <57392266+ozmld@users.noreply.github.com>
2022-12-20 00:21:42 +0300

    Update module5-taskD.cpp

--- module5-taskD.cpp
+++ module5-taskD.cpp
@@ -7,167 +7,178 @@ struct Node {
   Node* l;
   Node* r;
   Node() {}
-  Node(long long k, long long p)
-      : key(k), prior(p), size(1), l(nullptr), r(nullptr) {}
+  Node(long long k) {
+    /// For random priorities
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<long long> uni(1, 1e6);
+
+    prior = uni(rng);
+    key = k;
+    size = 1;
+    l = nullptr;
+    r = nullptr;
+  }
   Node(std::pair<long long, long long> node)
       : key(node.first), prior(node.second), size(0), l(nullptr), r(nullptr) {}
 };
+class Treap;
+class Treap {
+ public:
+  Treap() : root_(nullptr) {}
+  Treap(Node* t) : root_(t) {}
+  void Insert(Node* element) {
+    std::pair<Treap, Treap> splitted = Split(element->key);
+    root_ =
+        Merge(splitted.first.Merge(splitted.first, element), splitted.second)
+            .root_;
+  }
+  long long Kth(long long k) const {
+    if (root_ == nullptr) {
+      return -1;
+    }
+    if (LeftChild().Size() == (k - 1)) {
+      return root_->key;
+    }
+    if (LeftChild().Size() >= k) {
+      return LeftChild().Kth(k);
+    }
+    return RightChild().Kth(k - LeftChild().Size() - 1);
+  }
+  long long Next(long long x) const {
+    if (root_ == nullptr) {
+      return kInf;
+    }
+    if ((x + 1) == root_->key) {
+      return root_->key;
+    }
+    if ((x + 1) > root_->key) {
+      return RightChild().Next(x);
+    }
+    return std::min(LeftChild().Next(x), root_->key);
+  }
+  long long Prev(long long x) const {
+    if (root_ == nullptr) {
+      return -1;
+    }
+    if ((x - 1) == root_->key) {
+      return root_->key;
+    }
+    if ((x - 1) < root_->key) {
+      return LeftChild().Prev(x);
+    }
+    return std::max(RightChild().Prev(x), root_->key);
+  }
+  bool Find(long long x) const {
+    if (root_ == nullptr) {
+      return false;
+    }
+    if (root_->key == x) {
+      return true;
+    }
+    if (x > root_->key) {
+      return RightChild().Find(x);
+    }
+    return LeftChild().Find(x);
+  }
+  void Delete(int x) {
+    std::pair<Treap, Treap> splitted1 = Split(x);
+    std::pair<Treap, Treap> splitted2 = splitted1.second.Split(x + 1);
+    splitted2.first.Clear();
+    *this = Merge(splitted1.first, splitted2.second);
+  }
+  void Clear() {
+    if (root_ == nullptr) {
+      return;
+    }
+    LeftChild().Clear();
+    RightChild().Clear();
+    delete root_;
+  }
 
-long long Size(Node* t) {
-  if (t == nullptr) {
-    return 0;
+ private:
+  Node* root_;
+  long long Size() const {
+    if (root_ == nullptr) {
+      return 0;
+    }
+    return root_->size;
   }
-  return t->size;
-}
-void Update(Node* t) {
-  if (t != nullptr) {
-    t->size = 1 + Size(t->l) + Size(t->r);
+  void Update() {
+    if (root_ != nullptr) {
+      root_->size = 1 + LeftChild().Size() + RightChild().Size();
+    }
   }
-}
+  Treap Merge(Treap t1, Treap t2) {
+    if (t1.root_ == nullptr || t2.root_ == nullptr) {
+      return (t1.root_ == nullptr) ? t2 : t1;
+    }
+    if (t1.root_->prior > t2.root_->prior) {
+      t1.root_->r = Merge(Treap(t1.root_->r), t2).root_;
+      root_ = t1.root_;
+      Update();
+      return (Treap(root_));
+    }
+    t2.root_->l = Merge(t1, Treap(t2.root_->l)).root_;
+    root_ = t2.root_;
+    Update();
+    return (Treap(root_));
+  }
+  std::pair<Treap, Treap> Split(long long k) {
+    if (root_ == nullptr) {
+      return std::make_pair(Treap(), Treap());
+    }
+    if (k > root_->key) {
+      std::pair<Treap, Treap> splitted = RightChild().Split(k);
+      root_->r = splitted.first.root_;
+      Update();
+      return std::make_pair(*this, splitted.second);
+    }
+    std::pair<Treap, Treap> splitted = LeftChild().Split(k);
+    root_->l = splitted.second.root_;
+    Update();
+    return std::make_pair(splitted.first, *this);
+  }
+  Treap LeftChild() const { return Treap(root_->l); }
+  Treap RightChild() const { return Treap(root_->r); }
+};
 
-void Merge(Node*& t, Node* t1, Node* t2) {
-  if (t1 == nullptr || t2 == nullptr) {
-    t = (t1 == nullptr) ? t2 : t1;
-    return;
-  }
-  if (t1->prior > t2->prior) {
-    Merge(t1->r, t1->r, t2);
-    t = t1;
-    Update(t);
-    return;
-  }
-  Merge(t2->l, t1, t2->l);
-  t = t2;
-  Update(t);
-}
-
-void Split(Node* t, long long k, Node*& t1, Node*& t2) {
-  if (t == nullptr) {
-    t1 = nullptr;
-    t2 = nullptr;
-    return;
-  }
-  if (k > t->key) {
-    Split(t->r, k, t->r, t2);
-    t1 = t;
-    Update(t1);
-    return;
-  }
-  Split(t->l, k, t1, t->l);
-  t2 = t;
-  Update(t2);
-}
-void Insert(Node*& t, Node* element) {
-  Node *t1 = nullptr, *t2 = nullptr;
-  Split(t, element->key, t1, t2);
-  Merge(t1, t1, element);
-  Merge(t, t1, t2);
-}
-
-long long Kth(Node* t, long long k) {
-  if (t == nullptr) {
-    return -1;
-  }
-  if (Size(t->l) == (k - 1)) {
-    return t->key;
-  }
-  if (Size(t->l) >= k) {
-    return Kth(t->l, k);
-  }
-  return Kth(t->r, k - Size(t->l) - 1);
-}
-long long Next(Node* t, long long x) {
-  if (t == nullptr) {
-    return kInf;
-  }
-  if ((x + 1) == t->key) {
-    return t->key;
-  }
-  if ((x + 1) > t->key) {
-    return Next(t->r, x);
-  }
-  return std::min(Next(t->l, x), t->key);
-}
-long long Prev(Node* t, long long x) {
-  if (t == nullptr) {
-    return -1;
-  }
-  if ((x - 1) == t->key) {
-    return t->key;
-  }
-  if ((x - 1) < t->key) {
-    return Prev(t->l, x);
-  }
-  return std::max(Prev(t->r, x), t->key);
-}
-
-bool Find(Node* t, long long x) {
-  if (t == nullptr) {
-    return false;
-  }
-  if (t->key == x) {
-    return true;
-  }
-  if (x > t->key) {
-    return Find(t->r, x);
-  }
-  return Find(t->l, x);
-}
-void Delete(Node*& t) {
-  if (t == nullptr) {
-    return;
-  }
-  Delete(t->l);
-  Delete(t->r);
-  delete t;
-}
-void Delete(Node*& t, int x) {
-  Node *t1 = nullptr, *t2 = nullptr, *t3 = nullptr;
-  Split(t, x, t1, t2);
-  Split(t2, x + 1, t2, t3);
-  Delete(t2);
-  Merge(t, t1, t3);
-}
-std::string ParseOperation(Node*& t, std::string operation, long long x) {
-  std::random_device rd;
-  std::mt19937 rng(rd());
-  std::uniform_int_distribution<long long> uni(1, 1e6);
+std::string ParseOperation(Treap& treap, std::string operation, long long x) {
   if (operation == "insert") {
-    if (!Find(t, x)) {
-      long long r = uni(rng);
-      Node* elem = new Node(x, r);
-      Insert(t, elem);
+    if (!treap.Find(x)) {
+      Node* elem = new Node(x);
+      treap.Insert(elem);
     }
     return "";
   }
   if (operation == "exists") {
-    if (Find(t, x)) {
+    if (treap.Find(x)) {
       return "true\n";
     }
     return "false\n";
   }
   if (operation == "next") {
-    long long y = Next(t, x);
+    long long y = treap.Next(x);
     if (y == kInf) {
       return "none\n";
     }
     return std::to_string(y) + "\n";
   }
   if (operation == "prev") {
-    long long y = Prev(t, x);
+    long long y = treap.Prev(x);
     if (y == -1) {
       return "none\n";
     }
     return std::to_string(y) + "\n";
   }
   if (operation == "kth") {
-    long long y = Kth(t, x + 1);
+    long long y = treap.Kth(x + 1);
     if (y == -1) {
       return "none\n";
     }
     return std::to_string(y) + "\n";
   }
-  Delete(t, x);
+  treap.Delete(x);
   return "";
 }
 int main() {
@@ -175,10 +186,10 @@ int main() {
   std::ofstream os("output.txt");
   std::string operation;
   long long x;
-  Node* t = nullptr;
+  Treap treap;
   while (is >> operation >> x) {
-    os << ParseOperation(t, operation, x);
+    os << ParseOperation(treap, operation, x);
   }
   os.close();
-  Delete(t);
+  treap.Clear();
 }
