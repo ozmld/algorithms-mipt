aaa921f1e461d806ccfff5aa8e77d0452d135db8
ozmld <57392266+ozmld@users.noreply.github.com>
2022-12-21 15:28:01 +0300

    Update module5-taskD.cpp

--- module5-taskD.cpp
+++ module5-taskD.cpp
@@ -27,122 +27,134 @@ class Treap {
  public:
   Treap() : root_(nullptr) {}
   Treap(Node* t) : root_(t) {}
-  void Insert(Node* element) {
-    std::pair<Treap, Treap> splitted = Split(element->key);
-    root_ =
-        Merge(splitted.first.Merge(splitted.first, element), splitted.second)
-            .root_;
-  }
-  long long Kth(long long k) const {
-    if (root_ == nullptr) {
-      return -1;
-    }
-    if (LeftChild().Size() == (k - 1)) {
-      return root_->key;
-    }
-    if (LeftChild().Size() >= k) {
-      return LeftChild().Kth(k);
-    }
-    return RightChild().Kth(k - LeftChild().Size() - 1);
-  }
-  long long Next(long long x) const {
-    if (root_ == nullptr) {
-      return kInf;
-    }
-    if ((x + 1) == root_->key) {
-      return root_->key;
-    }
-    if ((x + 1) > root_->key) {
-      return RightChild().Next(x);
-    }
-    return std::min(LeftChild().Next(x), root_->key);
-  }
-  long long Prev(long long x) const {
-    if (root_ == nullptr) {
-      return -1;
-    }
-    if ((x - 1) == root_->key) {
-      return root_->key;
-    }
-    if ((x - 1) < root_->key) {
-      return LeftChild().Prev(x);
-    }
-    return std::max(RightChild().Prev(x), root_->key);
-  }
-  bool Find(long long x) const {
-    if (root_ == nullptr) {
-      return false;
-    }
-    if (root_->key == x) {
-      return true;
-    }
-    if (x > root_->key) {
-      return RightChild().Find(x);
-    }
-    return LeftChild().Find(x);
-  }
-  void Delete(int x) {
-    std::pair<Treap, Treap> splitted1 = Split(x);
-    std::pair<Treap, Treap> splitted2 = splitted1.second.Split(x + 1);
-    splitted2.first.Clear();
-    *this = Merge(splitted1.first, splitted2.second);
-  }
-  void Clear() {
-    if (root_ == nullptr) {
-      return;
-    }
-    LeftChild().Clear();
-    RightChild().Clear();
-    delete root_;
-  }
+  void Insert(Node* element);
+  long long Kth(long long k) const;
+  long long Next(long long x) const;
+  long long Prev(long long x) const;
+  bool Find(long long x) const;
+  void Delete(int x);
+  void Clear();
 
  private:
   Node* root_;
-  long long Size() const {
-    if (root_ == nullptr) {
-      return 0;
-    }
-    return root_->size;
+  long long Size() const;
+  void Update();
+  Treap Merge(Treap t1, Treap t2);
+  std::pair<Treap, Treap> Split(long long k);
+  Treap LeftChild() const;
+  Treap RightChild() const;
+};
+
+void Treap::Insert(Node* element) {
+  std::pair<Treap, Treap> splitted = Split(element->key);
+  root_ = Merge(splitted.first.Merge(splitted.first, element), splitted.second)
+              .root_;
+}
+long long Treap::Kth(long long k) const {
+  if (root_ == nullptr) {
+    return -1;
   }
-  void Update() {
-    if (root_ != nullptr) {
-      root_->size = 1 + LeftChild().Size() + RightChild().Size();
-    }
+  if (LeftChild().Size() == (k - 1)) {
+    return root_->key;
   }
-  Treap Merge(Treap t1, Treap t2) {
-    if (t1.root_ == nullptr || t2.root_ == nullptr) {
-      return (t1.root_ == nullptr) ? t2 : t1;
-    }
-    if (t1.root_->prior > t2.root_->prior) {
-      t1.root_->r = Merge(Treap(t1.root_->r), t2).root_;
-      root_ = t1.root_;
-      Update();
-      return (Treap(root_));
-    }
-    t2.root_->l = Merge(t1, Treap(t2.root_->l)).root_;
-    root_ = t2.root_;
+  if (LeftChild().Size() >= k) {
+    return LeftChild().Kth(k);
+  }
+  return RightChild().Kth(k - LeftChild().Size() - 1);
+}
+long long Treap::Next(long long x) const {
+  if (root_ == nullptr) {
+    return kInf;
+  }
+  if ((x + 1) == root_->key) {
+    return root_->key;
+  }
+  if ((x + 1) > root_->key) {
+    return RightChild().Next(x);
+  }
+  return std::min(LeftChild().Next(x), root_->key);
+}
+long long Treap::Prev(long long x) const {
+  if (root_ == nullptr) {
+    return -1;
+  }
+  if ((x - 1) == root_->key) {
+    return root_->key;
+  }
+  if ((x - 1) < root_->key) {
+    return LeftChild().Prev(x);
+  }
+  return std::max(RightChild().Prev(x), root_->key);
+}
+bool Treap::Find(long long x) const {
+  if (root_ == nullptr) {
+    return false;
+  }
+  if (root_->key == x) {
+    return true;
+  }
+  if (x > root_->key) {
+    return RightChild().Find(x);
+  }
+  return LeftChild().Find(x);
+}
+void Treap::Delete(int x) {
+  std::pair<Treap, Treap> splitted1 = Split(x);
+  std::pair<Treap, Treap> splitted2 = splitted1.second.Split(x + 1);
+  splitted2.first.Clear();
+  *this = Merge(splitted1.first, splitted2.second);
+}
+void Treap::Clear() {
+  if (root_ == nullptr) {
+    return;
+  }
+  LeftChild().Clear();
+  RightChild().Clear();
+  delete root_;
+}
+long long Treap::Size() const {
+  if (root_ == nullptr) {
+    return 0;
+  }
+  return root_->size;
+}
+void Treap::Update() {
+  if (root_ != nullptr) {
+    root_->size = 1 + LeftChild().Size() + RightChild().Size();
+  }
+}
+Treap Treap::Merge(Treap t1, Treap t2) {
+  if (t1.root_ == nullptr || t2.root_ == nullptr) {
+    return (t1.root_ == nullptr) ? t2 : t1;
+  }
+  if (t1.root_->prior > t2.root_->prior) {
+    t1.root_->r = Merge(Treap(t1.root_->r), t2).root_;
+    root_ = t1.root_;
     Update();
     return (Treap(root_));
   }
-  std::pair<Treap, Treap> Split(long long k) {
-    if (root_ == nullptr) {
-      return std::make_pair(Treap(), Treap());
-    }
-    if (k > root_->key) {
-      std::pair<Treap, Treap> splitted = RightChild().Split(k);
-      root_->r = splitted.first.root_;
-      Update();
-      return std::make_pair(*this, splitted.second);
-    }
-    std::pair<Treap, Treap> splitted = LeftChild().Split(k);
-    root_->l = splitted.second.root_;
-    Update();
-    return std::make_pair(splitted.first, *this);
+  t2.root_->l = Merge(t1, Treap(t2.root_->l)).root_;
+  root_ = t2.root_;
+  Update();
+  return (Treap(root_));
+}
+std::pair<Treap, Treap> Treap::Split(long long k) {
+  if (root_ == nullptr) {
+    return std::make_pair(Treap(), Treap());
   }
-  Treap LeftChild() const { return Treap(root_->l); }
-  Treap RightChild() const { return Treap(root_->r); }
-};
-
+  if (k > root_->key) {
+    std::pair<Treap, Treap> splitted = RightChild().Split(k);
+    root_->r = splitted.first.root_;
+    Update();
+    return std::make_pair(*this, splitted.second);
+  }
+  std::pair<Treap, Treap> splitted = LeftChild().Split(k);
+  root_->l = splitted.second.root_;
+  Update();
+  return std::make_pair(splitted.first, *this);
+}
+Treap Treap::LeftChild() const { return Treap(root_->l); }
+Treap Treap::RightChild() const { return Treap(root_->r); }
 std::string ParseOperation(Treap& treap, std::string operation, long long x) {
   if (operation == "insert") {
     if (!treap.Find(x)) {
