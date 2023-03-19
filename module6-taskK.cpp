10bf1013463ad8343d4739898572a6cd1a8eff40
ozmld <57392266+ozmld@users.noreply.github.com>
2023-03-19 04:41:38 +0300

    Update module6-taskK.cpp

--- module6-taskK.cpp
+++ module6-taskK.cpp
@@ -7,10 +7,36 @@
 #include <vector>
 
 struct Item {
-  int m, c;
-  Item(int m, int c) : m(m), c(c) {}
+  int weight, price;
+  Item(int weight, int price) : weight(weight), price(price) {}
 };
 
+std::vector<int> FindOptimalSet(int n, int m, std::vector<Item>& items) {
+  std::vector<std::vector<int>> dp(
+      n + 1, std::vector<int>(m + 1, 0));  // dp[i][j] - max value of items with
+                                           // max capacity j if we use first i
+                                           // items
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= m; ++j) {
+      if (j < items[i].weight) {
+        dp[i][j] = dp[i - 1][j];
+        continue;
+      }
+      dp[i][j] = std::max(dp[i - 1][j],
+                          dp[i - 1][j - items[i].weight] + items[i].price);
+    }
+  }
+  std::vector<int> optimal_set = {};
+  int i = n, j = m;
+  while (dp[i][j] != 0) {
+    if (dp[i][j] != dp[i - 1][j]) {
+      optimal_set.push_back(i);
+      j -= items[i].weight;
+    }
+    i--;
+  }
+  return optimal_set;
+}
 int main() {
   int n, m;
   std::cin >> n >> m;
@@ -23,27 +49,10 @@ int main() {
   for (int i = 0; i < n; ++i) {
     int c;
     std::cin >> c;
-    items[i + 1].c = c;
+    items[i + 1].price = c;
   }
-  std::vector<std::vector<int>> dp(
-      n + 1, std::vector<int>(m + 1, 0));  // dp[i][j] - max value of items with
-                                           // max capacity j if we use first i
-                                           // items
-  for (int i = 1; i <= n; ++i) {
-    for (int j = 1; j <= m; ++j) {
-      if (j < items[i].m) {
-        dp[i][j] = dp[i - 1][j];
-        continue;
-      }
-      dp[i][j] = std::max(dp[i - 1][j], dp[i - 1][j - items[i].m] + items[i].c);
-    }
-  }
-  int i = n, j = m;
-  while (dp[i][j] != 0) {
-    if (dp[i][j] != dp[i - 1][j]) {
-      std::cout << i << "\n";
-      j -= items[i].m;
-    }
-    i--;
+  std::vector<int> optimal_set = FindOptimalSet(n, m, items);
+  for (size_t i = 0; i < optimal_set.size(); ++i) {
+    std::cout << optimal_set[i] << "\n";
   }
 }
