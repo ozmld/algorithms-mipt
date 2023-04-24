92931fa87b955d1f214229221efba1b68a829736
ozmld <57392266+ozmld@users.noreply.github.com>
2023-04-24 19:34:56 +0300

    Update module7-taskE.cpp

--- module7-taskE.cpp
+++ module7-taskE.cpp
@@ -5,7 +5,6 @@
 #include <map>
 #include <algorithm>
 
-
 struct VertexInfo {
   unsigned short tin, ret, parent;
   unsigned short used;
@@ -14,30 +13,28 @@ struct VertexInfo {
 
 class Graph {
  public:
-  explicit Graph() : graph_({}) {}
   explicit Graph(size_t vertex_number) : graph_(vertex_number) {}
   unsigned short GetVertexNumber() const { return graph_.size(); }
   void AddEdge(unsigned short v, unsigned short u, size_t num) {
     graph_[v].push_back(u);
-    if (edges_.count({v, u}) == 0) {
-      edges_[{v, u}] = num;
+    if (edges_label_.count({v, u}) == 0) {
+      edges_label_[{v, u}] = num;
     } else {
-      edges_[{v, u}] = 0;
+      edges_label_[{v, u}] = 0;
     }
   }
   const std::vector<unsigned short>& GetAdjacentVertices(unsigned short v) const {
     return graph_[v];
-    
   }
   size_t GetEdgeNumber(unsigned short v, unsigned short u) const {
-    if (edges_.count({v, u}) != 0) {
-      return edges_.at({v, u});
+    if (edges_label_.count({v, u}) != 0) {
+      return edges_label_.at({v, u});
     }
     return 0;
   }
  private:
   std::vector<std::vector<unsigned short>> graph_;
-  std::map<std::pair<unsigned short, unsigned short>, size_t> edges_;
+  std::map<std::pair<unsigned short, unsigned short>, size_t> edges_label_;
 };
 
 void FindBridgesInComponent(const Graph& graph, unsigned short v, unsigned short timer,
